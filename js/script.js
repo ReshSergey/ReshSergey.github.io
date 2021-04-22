7d15596a6f41e7e066f99b8a01f1e713e67fa635
Sergey Resh <resh.s.v@yandex.ru>
2021-04-22 11:52:58 +0400

    Add Modal Windows

---
+++ js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        infinite: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    // MODAL
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, .modal').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+});
+
+
+// отключение табов чтоб не ползла верстка в каталоге
+function switchOff() {
+	var alltags = document.getElementsByTagName('*');
+	for (var i=0; i<alltags.length; i++)
+	{
+		if (alltags[i].tabIndex >= 0)
+		{
+			alltags[i].tabIndex = -2;
+		}
+	}
+}
+
+switchOff();;
